73fe85c6a89ec487a6e61d47f8fe621d2522f108
Samiser <github@me.samiser.xyz>
2025-12-04 12:41:43 +0000

    day 3 refined

--- 2025/aoc_2025/days/day03.py
+++ 2025/aoc_2025/days/day03.py
@@ -1,24 +1,26 @@
-def boop2(bank: list[int], count: int) -> int:
-    values: list[int] = []
-    max_pos = max(range(len(bank) - (count - 1)), key=bank.__getitem__)
-    values.append(bank[max_pos])
+def joltage(bank: list[int], count: int) -> int:
+    chosen: list[int] = []
+    max_pos = 0
 
-    for i in range(count - 1):
-        cut = (count - i) - 2
-        candidates = range(max_pos + 1, len(bank) - cut)
+    for i in range(count):
+        candidates = range(max_pos + int(i > 0), len(bank) - (count - i - 1))
         max_pos = max(candidates, key=bank.__getitem__)
-        values.append(bank[max(candidates, key=bank.__getitem__)])
+        chosen.append(bank[max_pos])
 
-    return int("".join([str(digit) for digit in values]))
+    return int("".join([str(digit) for digit in chosen]))
+
+
+def banks_to_joltages(banks: list[str], size: int):
+    return [joltage([int(x) for x in line], size) for line in banks]
+
+
+def solve(data: str, size: int):
+    return str(sum(banks_to_joltages(data.splitlines(), size)))
 
 
 def part1(data: str) -> str:
-    return str(
-        sum([boop2([int(x) for x in list(line)], 2) for line in data.splitlines()])
-    )
+    return solve(data, 2)
 
 
 def part2(data: str) -> str:
-    return str(
-        sum([boop2([int(x) for x in list(line)], 12) for line in data.splitlines()])
-    )
+    return solve(data, 12)
