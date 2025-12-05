2a498260ef1793ffe33cb70cb78786d2f51f86a4
Samiser <github@me.samiser.xyz>
2025-12-05 17:47:10 +0000

    optimised day 2, roughly halved the execution time

--- 2025/aoc_2025/days/day02.py
+++ 2025/aoc_2025/days/day02.py
@@ -1,41 +1,47 @@
-from typing import Callable
-
-IdValidator = Callable[[str], bool]
-
-
-def is_repeated_block(str_id: str, size: int) -> bool:
-    length = len(str_id)
-    if length % size != 0:
+def has_pair(s: str) -> bool:
+    length = len(s)
+    if length % 2 != 0:
         return False
+    half = length // 2
+    return s[:half] == s[half:]
 
-    block = str_id[:size]
-    return block * (length // size) == str_id
+
+def has_any_repeated_block(s: str) -> bool:
+    length = len(s)
+    limit = length // 2
+    for size in range(1, limit + 1):
+        if length % size != 0:
+            continue
+        block = s[:size]
+        if block * (length // size) == s:
+            return True
+    return False
 
 
-def count_invalid(data: str, is_invalid: IdValidator):
-    total = 0
+def count_invalid(data: str) -> tuple[str, str]:
+    pair_total = 0
+    repeated_block_total = 0
 
     for start, end in (s.split("-") for s in data.split(",")):
-        for id in range(int(start), int(end) + 1):
-            if is_invalid(str(id)):
-                total += id
+        for n in range(int(start), int(end) + 1):
+            s = str(n)
+            if has_pair(s):
+                pair_total += n
+                repeated_block_total += n
+                continue
+            if has_any_repeated_block(s):
+                repeated_block_total += n
 
-    return str(total)
+    return str(pair_total), str(repeated_block_total)
 
 
 def part1(data: str) -> str:
-    def has_pair(id: str) -> bool:
-        return len(id) % 2 == 0 and is_repeated_block(id, len(id) // 2)
-
-    return count_invalid(data, has_pair)
+    return count_invalid(data)[0]
 
 
 def part2(data: str) -> str:
-    def has_any_repeated_block(id: str) -> bool:
-        return any(is_repeated_block(id, size) for size in range(1, len(id)))
-
-    return count_invalid(data, has_any_repeated_block)
+    return count_invalid(data)[1]
 
 
 def solve(data: str) -> str:
-    return "\n".join((part1(data), part2(data)))
+    return "\n".join(count_invalid(data))
