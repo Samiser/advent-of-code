c3ad8440b530198c3d55947d174acb3bc850c6fb
Samiser <github@me.samiser.xyz>
2025-12-08 12:37:37 +0000

    day 7 improvements and optimisations

--- 2025/aoc_2025/days/day07.py
+++ 2025/aoc_2025/days/day07.py
@@ -1,23 +1,16 @@
 def solve(data: str) -> tuple[int, int]:
     lines = data.splitlines()
 
-    beam_positions: set[int] = set()
     split_count = 0
-    timeline_counts = [0] * len(lines[0])
-
-    beam_positions.add(lines[0].find("S"))
+    paths = [0] * len(lines[0])
+    paths[lines[0].find("S")] = 1
 
     for line in lines:
         for pos, char in enumerate(line):
-            if char == "^" and pos in beam_positions:
+            if char == "^" and paths[pos] > 0:
                 split_count += 1
-                beam_positions.add(pos - 1)
-                beam_positions.add(pos + 1)
-                beam_positions.remove(pos)
+                paths[pos - 1] += paths[pos]
+                paths[pos + 1] += paths[pos]
+                paths[pos] = 0
 
-                count = timeline_counts[pos]
-                timeline_counts[pos - 1] += count if count > 0 else 1
-                timeline_counts[pos + 1] += count if count > 0 else 1
-                timeline_counts[pos] = 0
-
-    return split_count, sum(timeline_counts)
+    return split_count, sum(paths)
